bdefda71318ea047457e1aa395ede646ad7a8580
cardiacinterventions <cardiacinterventionsj@gmail.com>
2020-06-05 11:33:38 +0530

    version 1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonService } from '../common.service';
+import { FooterComponent } from '../footer/footer.component';
+// import { ScrollingModule } from '@angular/cdk/scrolling';
+// import { ScrollDispatcher } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  someVariable:any
+  sticky:any
+  logo;
+  navbarOpen = false;
+  navbar: any
+  
+  @HostListener('window:scroll', ['$event']) 
+    scrollHandler(event) {
+      
+      
+      // this.sticky = true
+      
+    }
+    
+    
+  
+  constructor(private commSer: CommonService, private foo: FooterComponent) { }
 
   ngOnInit() {
+    this.sticky = true
   }
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+  getLogo(){
+    this.commSer.getLogos().subscribe(data => {
+      this.logo = data
+      console.log('data', data)
+    })
+  }
+  
 
 }
